b1f8393b46ddbd1daa25a833028a9a5c1331a60e
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-05-13 20:52:42 +0300

    demo changes

--- Tasks/Demo/client.cpp
+++ Tasks/Demo/client.cpp
@@ -2,56 +2,119 @@
 #include <iostream>
 #include <thread>
 
-using boost::asio::ip::tcp;
+#define BOLDWHITE "\033[1m\033[37m"
+#define RESET "\033[0m"
 
+using boost::asio::ip::tcp;
+using std::cout;
+using std::endl;
+using std::string;
+bool exit_flag = false;
+
+/**
+ * @brief Read a response from the server
+ *
+ * @param socket The socket to read from
+ * @return string The response from the server
+ */
+string read_response(tcp::socket& socket)
+{
+    boost::asio::streambuf buffer;
+    boost::asio::read_until(socket, buffer, "\n");
+    string response;
+    std::istream is(&buffer);
+    std::getline(is, response);
+    return response;
+}
+
+/**
+ * @brief Wait for messages from the server
+ *
+ * @param socket The socket to receive messages from
+ */
 void receive_messages(tcp::socket& socket)
 {
     try
     {
+        // Wait for messages from the server
         while (true)
         {
-            boost::asio::streambuf buffer;
-            boost::asio::read_until(socket, buffer, "\n");
-            std::string message;
-            std::istream is(&buffer);
-            std::getline(is, message);
-            std::cout << message << std::endl;
+            string response = read_response(socket);
+
+            cout << response << endl;
         }
     }
     catch (std::exception& e)
     {
-        std::cerr << "Exception in receive thread: " << e.what() << std::endl;
+        std::cerr << "thread: " << e.what() << endl;
     }
 }
 
+/**
+ * @brief Main function
+ *
+ * Connect to the server and handle user input
+ *
+ * @return int
+ */
 int main()
 {
     try
     {
         boost::asio::io_context io_context;
         tcp::resolver resolver(io_context);
+
         auto endpoints = resolver.resolve("localhost", "8080");
+
         tcp::socket socket(io_context);
         boost::asio::connect(socket, endpoints);
 
-        std::cout << "Enter your name: ";
-        std::string name;
-        std::getline(std::cin, name);
-        name += "\n";
+        while (true)
+        {
+            // Send the client's name to the server
+            cout << BOLDWHITE;
+            cout << "Enter your name: ";
+            cout << RESET;
 
-        boost::asio::write(socket, boost::asio::buffer(name));
+            string name;
+            std::getline(std::cin, name);
+            name += "\n";
 
+            boost::asio::write(socket, boost::asio::buffer(name));
+
+            // Wait for a response from the server to check if the name is valid
+            string response = read_response(socket);
+
+            if (response == "valid")
+            {
+                break;
+            }
+            else
+            {
+                cout << BOLDWHITE;
+                cout << "Ivalid name, please try again.";
+                cout << RESET << endl;
+            }
+        }
+
+        cout << BOLDWHITE;
+        cout << "Type 'exit' to leave the chat";
+        cout << RESET << endl;
+
+        // Start a thread to receive messages from the server
         std::thread receive_thread([&socket]()
                                    { receive_messages(socket); });
 
+        // Wait for user input
         while (true)
         {
-            std::string input;
+            string input;
             std::getline(std::cin, input);
 
             if (input == "exit")
             {
                 boost::asio::write(socket, boost::asio::buffer("exit\n"));
+                break;
             }
             else
             {
@@ -63,7 +126,7 @@ int main()
     }
     catch (std::exception& e)
     {
-        std::cerr << "Exception in main: " << e.what() << std::endl;
+        std::cerr << "main: " << e.what() << endl;
     }
 
     return 0;
--- Tasks/Demo/server.cpp
+++ Tasks/Demo/server.cpp
@@ -1,79 +1,174 @@
 #include <boost/asio.hpp>
 #include <iostream>
 #include <map>
+#include <mutex>
 #include <string>
 #include <thread>
 
+#define BOLDWHITE "\033[1m\033[37m"
+#define RESET "\033[0m"
+
 using boost::asio::ip::tcp;
+using std::cout;
+using std::endl;
+using std::string;
 
-std::map<std::string, tcp::socket*> clients;
+std::map<string, tcp::socket*> clients;
 std::mutex mtx;
+int client_count = 0;
 
-void broadcast(const std::string& message, std::string sender)
+void handle_disconnect(const string&);
+
+/**
+ * @brief Broadcast types
+ */
+enum broadcast_type
 {
-    std::lock_guard<std::mutex> guard(mtx);
-    for (auto& pair : clients)
+    Message,
+    Announcement,
+};
+
+/**
+ * @brief Broadcast a message to connected clients
+ *
+ * Format message data and forward it to appropriate clients
+ *
+ * @param data The data to be broadcasted
+ * @param sender The name of the sender
+ * @param broadcast_type The type of broadcast
+ */
+void broadcast(const string& data, const string& sender, broadcast_type broadcast_type)
+{
+    string message;
+    bool exclude;
+
+    // Format message
+    switch (broadcast_type)
     {
-        if (pair.first != sender)
-        {
-            boost::asio::write(*(pair.second), boost::asio::buffer(message + "\n"));
-        }
+    case Message:
+        message = sender + ": " + data;
+        exclude = true;
+        cout << message << endl;
+        break;
+
+    case Announcement:
+        message = BOLDWHITE + data + RESET;
+        exclude = false;
+        cout << data << endl;
+        break;
     }
-    std::cout << message << std::endl;
+
+    // Send message clients
+    std::lock_guard<std::mutex> guard(mtx);
+    for (auto& client : clients)
+    {
+        if (exclude && client.first == sender)
+            continue;
+
+        boost::asio::write(*client.second, boost::asio::buffer(message + "\n"));
+    }
 }
 
+/**
+ * @brief Handle client connections
+ *
+ * Read client name and messages
+ *
+ * @param socket The client socket
+ */
 void handle_client(tcp::socket* socket)
 {
+    string client_name;
     try
     {
         boost::asio::streambuf buffer;
-        std::string client_name;
 
-        while (client_name.empty())
+        // Get client name
+        while (true)
         {
             boost::asio::read_until(*socket, buffer, "\n");
             std::istream is(&buffer);
             std::getline(is, client_name);
-        }
 
-        {
-            std::lock_guard<std::mutex> guard(mtx);
-            clients[client_name] = socket;
-        }
-
-        broadcast(client_name + " online", client_name);
-
-        while (true)
-        {
-            boost::asio::read_until(*socket, buffer, "\n");
-            std::string message;
-            std::istream is(&buffer);
-            std::getline(is, message);
-
-            if (message == "exit")
+            // handle duplicate names
             {
+                std::lock_guard<std::mutex> guard(mtx);
+                if (clients.find(client_name) != clients.end())
                 {
-                    std::lock_guard<std::mutex> guard(mtx);
-                    broadcast(client_name + " offline", client_name);
+                    boost::asio::write(*socket, boost::asio::buffer("invalid\n"));
+                    client_name.clear();
                 }
-                break;
-            }
-            else if (message != "")
-            {
-                broadcast(client_name + ": " + message, client_name);
+                else
+                {
+                    boost::asio::write(*socket, boost::asio::buffer("valid\n"));
+                    break;
+                }
             }
             buffer.consume(buffer.size());
         }
 
-        clients.erase(client_name);
-        delete socket;
+        // Add client to the list
+        {
+            std::lock_guard<std::mutex> guard(mtx);
+            client_count++;
+            clients[client_name] = socket;
+        }
+
+        broadcast(client_name + " joined, ONLINE: " + std::to_string(client_count), client_name, Announcement);
+
+        // Wait for messages from the client
+        while (true)
+        {
+            boost::asio::read_until(*socket, buffer, "\n");
+            string message;
+            std::istream is(&buffer);
+            std::getline(is, message);
+
+            if (message == "exit")
+                break;
+
+            else if (message != "")
+            {
+                broadcast(message, client_name, Message);
+            }
+            buffer.consume(buffer.size());
+        }
+
+        handle_disconnect(client_name);
     }
     catch (std::exception& e)
     {
-        std::cerr << "Exception in thread: " << e.what() << std::endl;
+        std::cerr << "thread: " << e.what() << endl;
+        handle_disconnect(client_name);
     }
 }
 
+/**
+ * @brief Handle client disconnects
+ *
+ * @param client_name The name of the client
+ */
+void handle_disconnect(const string& client_name)
+{
+    {
+        std::lock_guard<std::mutex> guard(mtx);
+        client_count--;
+
+        broadcast(client_name + " left, ONLINE: " + std::to_string(client_count), client_name, Announcement);
+
+        // delete socket
+        delete clients[client_name];
+        clients.erase(client_name);
+    }
+}
+
+/**
+ * @brief Main function
+ *
+ * Accept clients and create a new thread for each
+ *
+ * @return int
+ */
 int main()
 {
     boost::asio::io_context io_context;
@@ -91,7 +186,7 @@ int main()
         }
         catch (std::exception& e)
         {
-            std::cerr << "Exception in main: " << e.what() << std::endl;
+            std::cerr << "main: " << e.what() << endl;
         }
     }
 
